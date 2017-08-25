178a89f5f29ed37886423477dafb8f5a9b86abaa
Liezl Santos <lsantos@freelancer.com>
2017-08-25 10:52:51 +0800

    upgrade to angular 4

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -4,11 +4,8 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
 import { Component } from '@angular/core';
-export var AppComponent = (function () {
+var AppComponent = (function () {
     function AppComponent() {
         this.name = 'Angular';
     }
@@ -16,9 +13,9 @@ export var AppComponent = (function () {
         Component({
             selector: 'my-app',
             templateUrl: './app.component.html'
-        }), 
-        __metadata('design:paramtypes', [])
+        })
     ], AppComponent);
     return AppComponent;
 }());
+export { AppComponent };
 //# sourceMappingURL=app.component.js.map
--- src/app/app.module.js
+++ src/app/app.module.js
@@ -4,13 +4,10 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-export var AppModule = (function () {
+var AppModule = (function () {
     function AppModule() {
     }
     AppModule = __decorate([
@@ -18,9 +15,9 @@ export var AppModule = (function () {
             imports: [BrowserModule],
             declarations: [AppComponent],
             bootstrap: [AppComponent]
-        }), 
-        __metadata('design:paramtypes', [])
+        })
     ], AppModule);
     return AppModule;
 }());
+export { AppModule };
 //# sourceMappingURL=app.module.js.map
